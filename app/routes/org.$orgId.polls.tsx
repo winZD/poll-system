1300f7f4a1969e47fbbc924962432c771045c5ec
winZD <puntica007@gmail.com>
2024-09-04 10:53:20 +0200

    poll

--- app/routes/org.$orgId.polls.tsx
+++ app/routes/org.$orgId.polls.tsx
@@ -1,3 +1,32 @@
+import {
+  LoaderFunctionArgs,
+  json,
+  ActionFunctionArgs,
+  redirect,
+} from '@remix-run/node';
+import { decodeTokenFromRequest } from '~/utils';
+import { db } from '~/utils/db';
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  // TODO: decode token
+  console.log(request.headers);
+  const ctx = await decodeTokenFromRequest(request);
+
+  if (!ctx) return redirect('/login');
+
+  //TODO: check if admin is logged in
+
+  const activeOrgs = await db.pollTable.findMany({
+    where: { orgId: '', userId: '' },
+  });
+
+  return json(activeOrgs);
+}
+
+export const action = async ({ request, params }: ActionFunctionArgs) => {
+  return json({});
+};
+
 export default function Index() {
   return <div className="text-red-700">Lista anketa</div>;
 }
