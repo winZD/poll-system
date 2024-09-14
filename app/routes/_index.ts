c89ea4e6b395deb3566331cd19f58c4534edaf86
jrglvn <jrglvn@yahoo.com>
2024-09-14 20:52:43 +0200

    redirecting on index

---
+++ app/routes/_index.ts
@@ -0,0 +1,18 @@
+import { LoaderFunctionArgs, redirect } from '@remix-run/node';
+
+import { getUserFromRequest } from '~/auth';
+import { roleValues } from '~/components/models';
+
+export async function loader({ request, params }: LoaderFunctionArgs) {
+  // const token = decode
+
+  const user = await getUserFromRequest(request);
+
+  if (!user) return null;
+
+  return redirect(
+    user?.Org.role === roleValues.ADMIN
+      ? '/app/admin'
+      : `/app/org/${user?.Org.id}`,
+  );
+}
