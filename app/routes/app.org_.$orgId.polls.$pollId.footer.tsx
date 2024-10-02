69593d3f7d2f02ed43daa53980c7b6c0dda5c4b4
winZD <puntica007@gmail.com>
2024-10-02 17:44:28 +0200

    fixed link in QR code

--- app/routes/app.org_.$orgId.polls.$pollId.footer.tsx
+++ app/routes/app.org_.$orgId.polls.$pollId.footer.tsx
@@ -1,24 +1,27 @@
 import { LoaderFunctionArgs } from '@remix-run/node';
-import { useLoaderData } from '@remix-run/react';
+import { useLoaderData, useParams } from '@remix-run/react';
 import { QRCodeSVG } from 'qrcode.react';
 import { PollChartWithVotes } from '~/components/PollChartWithVotes';
 import { getPollData } from '~/functions/getPollData';
 
 export async function loader({ request, params }: LoaderFunctionArgs) {
   const { pollId, orgId } = params;
+  const url = new URL(request.url);
+  const baseUrl = `${url.protocol}//${url.host}`;
 
   const { poll, votes } = await getPollData({
     pollId: pollId as string,
     orgId: orgId as string,
   });
 
-  return { poll, votes };
+  return { poll, votes, baseUrl };
 }
 
 export type PollLoaderType = typeof loader;
 
 const Index = () => {
-  const { poll } = useLoaderData<typeof loader>();
+  const { baseUrl } = useLoaderData<typeof loader>();
+  const { orgId, pollId } = useParams();
 
   return (
     <div className="flex aspect-video w-[1920px] flex-col justify-end border-2 p-8">
@@ -28,7 +31,7 @@ const Index = () => {
 
       <div className="flex items-center justify-end gap-16 text-lg">
         <PollChartWithVotes />
-        <QRCodeSVG size={128} value={poll.iframeSrc} />
+        <QRCodeSVG size={128} value={`${baseUrl}/poll/${orgId}/${pollId}`} />
       </div>
     </div>
   );
